2cf135620bddf56e50852d03e92f404d955cbf0c
a1denvalu3 <43107113+a1denvalu3@users.noreply.github.com>
2026-06-11 11:00:43 +0200

    Update build.gradle.kts (#342)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.electricdreams.numo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 18
+        versionCode = 19
         versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
